fc224f31152f27ff1d2f72297926ac4c0fd9e5b7
Mohit <mohit.punglia@gmail.com>
2024-11-28 22:01:07 +0000

    few_shot.py file added

---
+++ few_shot.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import json
+
+
+class FewShotPosts:
+    def __init__(self, file_path="data/processed_posts.json"):
+        self.df = None
+        self.unique_tags = None
+        self.load_posts(file_path)
