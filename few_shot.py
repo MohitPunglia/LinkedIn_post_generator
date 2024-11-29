c5b8acc185eb35dc88c11610c553e29d544c028d
Mohit <mohit.punglia@gmail.com>
2024-11-29 22:03:16 +0000

    load_posts method added

--- few_shot.py
+++ few_shot.py
@@ -7,3 +7,11 @@ class FewShotPosts:
         self.df = None
         self.unique_tags = None
         self.load_posts(file_path)
+
+    def load_posts(self, file_path):
+        with open(file_path, encoding="utf-8") as f:
+            posts = json.load(f)
+            self.df = pd.json_normalize(posts)
+            self.df["length"] = self.df["line_count"].apply(self.categorize_length)
+            all_tags = self.df["tags"].apply(lambda x: x).sum()
+            self.unique_tags = list(set(all_tags))
